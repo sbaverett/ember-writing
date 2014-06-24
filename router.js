1caba486ffea72b300310e46a15cb4bcc7ed3d8f
J. Allen Tucker <j.allentucker@gmail.com>
2014-06-24 16:47:22 -0700

    working on changing model object into array of objects to iterate over

--- router.js
+++ router.js
@@ -2,13 +2,23 @@ App.Router.map(function() {
 	this.resource('numbers', function() {
 		this.route('one');
   	this.route('two');
-	}); 
+	});
 });
 
-App.NumbersOneRoute = Ember.Route.extend({
+App.NumbersRoute = Ember.Route.extend({
 	model: function() {
-		return {
-			path: 'one.jpeg'
-		};
+		return [{
+			path: 'one.jpeg',
+      french: 'Un',
+      url: 'en.wikipedia.org/wiki/one',
+      linkName: 'One',
+      routePath: 'numbers.one'
+		}, {
+      path: 'two.jpeg',
+      french: 'deux',
+      url: 'en.wikipedia.org/wiki/two',
+      linkName: 'Two',
+      routePath: 'numbers.two'
+    }];
 	}
 });
