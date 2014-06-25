455d0cdd8582872e078a19be5cfe62e0e729dd51
Sam Averett <averettsam@comcast.net>
2014-06-25 15:35:00 -0700

    added edit function

--- router.js
+++ router.js
@@ -1,18 +1,19 @@
 App.Router.map(function() {
 	this.resource('numbers');
   this.route('number', { path: '/numbers/:numbers_id' });
+  this.route('edits', { path: '/numbers/:numbers_id/edits' });
 });
 
 var numbersArray = [{
       path: 'one.jpeg',
-      french: 'Un',
+      anotherLanguage: 'Un',
       url: 'http://en.wikipedia.org/wiki/one',
       linkName: 'One',
       routePath: 'numbers.one',
       id: 1
     }, {
       path: 'two.jpeg',
-      french: 'deux',
+      anotherLanguage: 'deux',
       url: 'http://en.wikipedia.org/wiki/two',
       linkName: 'Two',
       routePath: 'numbers.two',
@@ -26,6 +27,17 @@ App.NumbersRoute = Ember.Route.extend({
 });
 
 App.NumberRoute = Ember.Route.extend({
+	model: function(params) {
+		var numberObject;
+		numbersArray.forEach(function(object) {
+			if(object.id === +params.numbers_id) {
+				numberObject = object;
+			}
+		});
+		return numberObject;
+	}
+});
+App.EditsRoute = Ember.Route.extend({
 	model: function(params) {
 		var numberObject;
 		numbersArray.forEach(function(object) {
