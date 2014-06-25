065903c042f49df7075007a83e4ae37a9b78fd7e
J. Allen Tucker <j.allentucker@gmail.com>
2014-06-25 11:18:46 -0700

    made index.html more modular

--- router.js
+++ router.js
@@ -1,24 +1,27 @@
 App.Router.map(function() {
-	this.resource('numbers', function() {
-		this.route('one');
-  	this.route('two');
-	});
+	this.resource('numbers');
+  this.route('number', { path: '/numbers/:numbers_id' });
 });
 
-App.NumbersRoute = Ember.Route.extend({
-	model: function() {
-		return [{
-			path: 'one.jpeg',
+var numbersArray = [{
+      path: 'one.jpeg',
       french: 'Un',
       url: 'en.wikipedia.org/wiki/one',
       linkName: 'One',
-      routePath: 'numbers.one'
-		}, {
+      routePath: 'numbers.one',
+      id: 1
+    }, {
       path: 'two.jpeg',
       french: 'deux',
       url: 'en.wikipedia.org/wiki/two',
       linkName: 'Two',
-      routePath: 'numbers.two'
+      routePath: 'numbers.two',
+      id: 2
     }];
+
+App.NumbersRoute = Ember.Route.extend({
+	model: function() {
+		return numbersArray;
 	}
 });
+
