6c3bbe266069e6e73b4a06ac7609f3993ae6e6b8
Carlo De Gregorio <cdegregorio@gmail.com>
2014-06-25 11:41:44 -0700

    fixed url link-to

--- router.js
+++ router.js
@@ -6,14 +6,14 @@ App.Router.map(function() {
 var numbersArray = [{
       path: 'one.jpeg',
       french: 'Un',
-      url: 'en.wikipedia.org/wiki/one',
+      url: 'http://en.wikipedia.org/wiki/one',
       linkName: 'One',
       routePath: 'numbers.one',
       id: 1
     }, {
       path: 'two.jpeg',
       french: 'deux',
-      url: 'en.wikipedia.org/wiki/two',
+      url: 'http://en.wikipedia.org/wiki/two',
       linkName: 'Two',
       routePath: 'numbers.two',
       id: 2
